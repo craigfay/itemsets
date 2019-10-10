4476686160d851db764c10cec7b998ccf94ba86e
Craig Fay <craigfay1@gmail.com>
2019-10-10 18:32:28 -0500

    Ensure unique cart

--- seed.js
+++ seed.js
@@ -1,26 +1,20 @@
 const fs = require('fs');
+const itemCatalog = require('./catalog')
 
 function randomBetween(min, max) {
   return Math.round(Math.random() * (max - min) + min);
 }
 
-const itemCatalog = [
-  'shoes',
-  'shirt',
-  'sock',
-  'scarf',
-  'sweater',
-  'sportcoat',
-  'bolo tie',
-];
-
 function seedOrderData(n) {
   let orders = [];
   for (let id = 0; id < n; id++) {
     let cart = [];
-    let itemCount = randomBetween(1, itemCatalog.length);
+    let unselectedItems = [...itemCatalog];
+    let itemCount = randomBetween(1, unselectedItems.length);
     for (let i = 0; i < itemCount; i++) {
-      cart.push(itemCatalog[randomBetween(0, itemCatalog.length - 1)]);
+      let chosenIndex = randomBetween(0, unselectedItems.length - 1);
+      cart.push(unselectedItems[chosenIndex]);
+      unselectedItems.splice(chosenIndex, 1);
     }
     orders.push({ id, cart });
   }
